61fbc2b56d669ffdd89833c7e90cb818cca4b392
Segment Git Sync <friends@segment.com>
2024-07-05 19:06:40 +0000

    User '9nHzbYrnjtufNgyRf7HJA4' updated by evild03r@bugcrowdninja.com

--- users/9nHzbYrnjtufNgyRf7HJA4.tf
+++ users/9nHzbYrnjtufNgyRf7HJA4.tf
@@ -15,7 +15,7 @@ resource "segment_user" "id-9nHzbYrnjtufNgyRf7HJA4" {
           type = "WORKSPACE"
         },
       ]
-      role_id = "1T4aB9og7L00rOWdpKsvJ0I3xDL"
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
     },
   ]
 }
