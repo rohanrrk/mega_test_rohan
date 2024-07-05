8fe54308c76b6639045f1c494a11203194f97145
Segment Git Sync <friends@segment.com>
2024-07-05 18:54:58 +0000

    User '9nHzbYrnjtufNgyRf7HJA4' updated by evild03r@bugcrowdninja.com

--- users/9nHzbYrnjtufNgyRf7HJA4.tf
+++ users/9nHzbYrnjtufNgyRf7HJA4.tf
@@ -15,6 +15,50 @@ resource "segment_user" "id-9nHzbYrnjtufNgyRf7HJA4" {
           type = "WORKSPACE"
         },
       ]
+      role_id = "1Mc8DHU42rYyIp94oBPexlvYpf8"
+    },
+    {
+      resources = [
+        {
+          id = "r6UY6oUJ67jL4qSWjxLM5D"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DJJa7MQ9L8HiZz5NL4QMgdj"
+    },
+    {
+      resources = [
+        {
+          id = "r6UY6oUJ67jL4qSWjxLM5D"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKQ95UMoCKw1y9syxModCaC"
+    },
+    {
+      resources = [
+        {
+          id = "r6UY6oUJ67jL4qSWjxLM5D"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKqCj3vrDvNml9Vhsc7Aebt"
+    },
+    {
+      resources = [
+        {
+          id = "r6UY6oUJ67jL4qSWjxLM5D"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
       role_id = "1Mc8DLRqfeBpxN5cNBjK0n4oTPX"
     },
     {
