58faefa460a9965cb59566beec879de233edb60a
Segment Git Sync <friends@segment.com>
2024-07-05 18:41:18 +0000

    Full sync

---
+++ labels/kllk_lklk.tf
@@ -0,0 +1,10 @@
+import {
+  to = segment_label.id-kllk_lklk
+  id = "kllk:lklk"
+}
+
+resource "segment_label" "id-kllk_lklk" {
+  description = null
+  key         = "kllk"
+  value       = "lklk"
+}
